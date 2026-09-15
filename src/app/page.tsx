daea60502db64e705582d03d7fefc01e8b481f6a
Enisco29 <opeyemioluwafisayo29@gmail.com>
2026-09-15 22:35:53 +0100

    added analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 import { Game } from "@/components/Game";
+import { Analytics } from "@vercel/analytics/next";
 
-export default function Home() { return <Game />; }
+export default function Home() {
+  return (
+    <>
+      <Analytics />
+      <Game /> ;
+    </>
+  );
+}
